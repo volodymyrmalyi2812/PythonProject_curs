b38da57c02fb706d5fd6e653fde833309c9836a9
Volodymyr Malyi <vladimir28122007@gmail.com>
2026-06-25 15:32:48 +0200

    i did lesson tasks for oop lesson 2

--- Python Essential/002.py
+++ Python Essential/002.py
@@ -29,19 +29,45 @@ class Laptop(Computer):
         self.model = model
 '''
 
+import datetime
 
-class Computer:
-    def __init__(self, name, amount, price, model, date_received_at_warehouse):
+class Product:
+    def __init__(self, name):
         self.name = name
+
+
+class SpecificProduct(Product):
+    def __init__(self, name, amount, price, model, date_received_at_warehouse):
+        super().__init__(name)
+
         self.amount = amount
         self.price = price
         self.model = model
         self.date_received_at_warehouse = date_received_at_warehouse
 
+    def __str__(self):
+        return f"{self.name} {self.amount} {self.price} {self.model}, {self.date_received_at_warehouse}"
 
-class Count_computers(Computer):
-    def __init__(self, name, amount, price, model, date_received_at_warehouse):
-        super().__init__(name, amount, price, model, date_received_at_warehouse)
+first_product = SpecificProduct("macbook", 5, 1500, "Pro", datetime.datetime(2026, 6, 20))
+second_product = SpecificProduct("Asus", 2, 1400, "Rog", datetime.datetime(2026, 6, 4))
+third_product = SpecificProduct("Acer", 10, 1100, "Nitro", datetime.datetime(2026, 5, 29))
 
-        if amount > 5:
-            return self
+products = [first_product, second_product, third_product]
+
+def find_specific_product(all_products):
+    product_result = []
+    for product in all_products:
+        if product.amount > 5:
+            product_result.append(product)
+
+    return product_result
+
+result = find_specific_product(products)
+
+for product in result:
+    print(f"product name: {product.name}")
+    print(f" amount: {product.amount}")
+    print(f" price: {product.price}")
+    print(f" model: {product.model}")
+    print(f" date received at warehouse: {product.date_received_at_warehouse}")
+
