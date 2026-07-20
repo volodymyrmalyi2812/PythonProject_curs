cad2731e50c3237414c84f27157d95c895d779b4
Vladimir Malyi <vladimir28122007@gmail.com>
2026-07-20 23:08:49 +0200

    compleated oop lesson 2

--- Python Essential/002/tasks/002_homework.py
+++ Python Essential/002/tasks/002_homework.py
@@ -148,3 +148,146 @@ from pyclbr import Class
 """
 
 
+# class Person:
+#     def __init__(self, surname, name, age):
+#         self.surname = surname
+#         self.name = name
+#         self.age = age
+#
+# class Ukraine(Person):
+#     def __init__(self, surname, name, age):
+#         super().__init__(surname, name, age)
+#
+# class USA(Person):
+#     def __init__(self, surname, name, age):
+#         super().__init__(surname, name, age)
+#
+# class CheckAge(Ukraine, USA):
+#     def __init__(self, surname, name, age):
+#         super().__init__(surname, name, age)
+#
+#     @staticmethod
+#     def check_age_in_ukraine(age):
+#         return age >= 18
+#
+#     @staticmethod
+#     def check_age_in_usa(age):
+#         return age >= 21
+#
+#
+# person = CheckAge("surname_1", "name_1", 20)
+#
+# print("Adult in Ukraine", CheckAge.check_age_in_ukraine(person.age))
+# print("Adult in USA", CheckAge.check_age_in_usa(person.age))
+
+
+"""
+Завдання 6
+
+Використовуючи код example_10, створіть декоратори @classmethod для формування переліку об'єктів, 
+які підрахують кількість повнолітніх людей в Україні та Америці.
+"""
+
+
+
+
+# class Person:
+#     people = []
+#
+#     def __init__(self, surname, name, age, country):
+#         self.surname = surname
+#         self.name = name
+#         self.age = age
+#         self.country = country
+#
+#         Person.people.append(self)
+#
+#     @classmethod
+#     def get_people_in_ukraine(cls):
+#         count = 0
+#
+#         for person in cls.people:
+#             if person.country == "Ukraine" and person.age >= 18:
+#                 count += 1
+#
+#         return count
+#
+#     @classmethod
+#     def get_people_in_usa(cls):
+#         count = 0
+#
+#         for person in cls.people:
+#             if person.country == "USA" and person.age >= 21:
+#                 count += 1
+#
+#         return count
+#
+# person1 = Person("surname1", "name1", 18, "Ukraine")
+# person2 = Person("surname2", "name2", 16, "USA")
+# person3 = Person("surname3", "name3", 24, "Ukraine")
+# person4 = Person("surname4", "name4", 30, "USA")
+#
+#
+#
+# print(Person.get_people_in_ukraine())
+# print(person1.get_people_in_usa())
+
+
+
+"""
+Завдання 7
+
+Створіть ієрархію класів транспортних засобів. 
+У загальному класі опишіть загальні всім транспортних засобів поля, у спадкоємцях – специфічні їм. 
+Створіть кілька екземплярів. 
+Виведіть інформацію щодо кожного транспортного засобу.
+"""
+
+
+
+
+class Transport:
+    def __init__(self, transport_type, wheels, engine, speed, weight):
+        self.transport_type = transport_type
+        self.wheels = wheels
+        self.engine = engine
+        self.speed = speed
+        self.weight = weight
+
+    def show_info(self):
+        print(self.transport_type, self.wheels, self.engine)
+
+class Bus(Transport):
+    def __init__(self, transport_type, wheels, engine, speed, weight, amount_of_passengers):
+        super().__init__(transport_type, wheels, engine, speed, weight)
+        self.amount_of_passengers = amount_of_passengers
+
+    def show_info(self):
+        print(f" type {self.transport_type}, wheels {self.wheels}, engine {self.engine}, top speed {self.speed},  weight {self.weight}, passengers {self.amount_of_passengers}")
+
+
+class Car(Transport):
+    def __init__(self, transport_type, wheels, engine, speed, weight, amount_of_doors):
+        super().__init__(transport_type, wheels, engine, speed, weight)
+        self.amount_of_doors = amount_of_doors
+
+    def show_info(self):
+        print(f" type {self.transport_type}, wheels {self.wheels}, engine {self.engine}, top speed {self.speed},  weight {self.weight}, doors {self.amount_of_doors}")
+
+
+
+class Cabrio(Car):
+    def __init__(self, transport_type, wheels, engine, speed, weight, amount_of_doors, type_of_roof):
+        super().__init__(transport_type, wheels, engine, speed, weight, amount_of_doors)
+        self.type_of_roof = type_of_roof
+
+    def show_info(self):
+        print(f" type {self.transport_type}, wheels {self.wheels}, engine {self.engine}, top speed {self.speed},  weight {self.weight}, doors {self.amount_of_doors},type_of_roof {self.type_of_roof}")
+
+
+bus = Bus("Bus", 4, "diesel", 90, 8000, 15)
+car = Car("Car", 4, "petrol", 250, 1500, 2)
+cabrio = Cabrio("Cabrio", 4, "petrol", 190, 1800, 2, "open")
+bus.show_info()
+car.show_info()
+cabrio.show_info()
